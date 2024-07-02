3d830259ad08c99c6e64b53b49220a50d070b262
unknown <kongnyuroger711@gmail.com>
2024-07-02 12:55:48 +0100

    created base templates

--- routes.py
+++ routes.py
@@ -4,3 +4,19 @@ def register_routes(app, db):
     @app.route('/')
     def index():
         return render_template('index.html')
+    
+    @app.route('/create_book')
+    def create_book():
+        return render_template('create-book.html')
+
+    @app.route('/profile')
+    def profile():
+        return render_template('profile.html')
+    
+    @app.route('/all_books')
+    def all_books():
+        return render_template('all_books.html')
+    
+    @app.route('/login')
+    def login():
+        return render_template('login.html')
